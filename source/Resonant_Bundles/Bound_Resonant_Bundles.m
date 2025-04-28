6035be9b681bd864266b9a5189fa0913920d2468
Jonathan Jaquette <JCJaquette@gmail.com>
2025-04-28 14:37:09 -0400

    Deleting ROO files

--- source/Resonant_Bundles/Bound_Resonant_Bundles.m
+++ source/Resonant_Bundles/Bound_Resonant_Bundles.m
@@ -12,6 +12,9 @@ function [outputArg1,outputArg2] = Bound_Resonant_Bundles(inputArg1,inputArg2)
     % Complicated L^-1 * L norm
 % Z_1 Bound vcx
 
+% Y_0-hat bound. Easy
+% Z_1-hat bound. Easy
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 end
